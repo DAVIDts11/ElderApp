27b67d7b4cbd250f4747206ad29cd61520175520
DAVID <tsiba1@walla.co.il>
2021-06-22 23:21:36 +0300

    home vo ,home mem

--- Screens/HomePageScreenMem.js
+++ Screens/HomePageScreenMem.js
@@ -1,17 +1,81 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from "react-redux";
 
 
-const HomepageMember = () => {
+const HomepageMember = ({ navigation }) => {
     const { currentUser } = useSelector(state => state.user);
-    console.log("store user = ",currentUser );
-  
+    console.log("store user = ", currentUser);
+    const onPress=()=>console.log("pressed");
     return (
-        <View>
+        <View style={styles.contener} >
             <Text>Hello member {currentUser.email}  !!</Text>
+            <View style={styles.row} >
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text> lala1   </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text> lala2   </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text> lala3   </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text>  lala4  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text>  lala4  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={onPress}
+                >
+                    <Text>  lala4  </Text>
+                </TouchableOpacity>
+
+            </View>
         </View>
     )
 }
 
+
+
+
+const styles = StyleSheet.create({
+    contener: {
+        flex: 1,
+        backgroundColor: "peachpuff",
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingTop: Platform.OS === "android" ? 25 : 0,
+    },
+    row: {
+        justifyContent: "space-around",
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+    button: {
+        display: "flex",
+        marginTop: 15,
+        height: 140,
+        width: 140,
+        backgroundColor: "red"
+    }
+
+});
 export default HomepageMember;
--- Screens/HomePageScreenVo.js
+++ Screens/HomePageScreenVo.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from "react-redux";
 
 
-const HomepageVolunteer = () => {
+const HomepageVolunteer = ({ navigation }) => {
     const { currentUser } = useSelector(state => state.user);
-    console.log("store user = ",currentUser );
-  
+    console.log("store user = ", currentUser);
+    const onPress = () => console.log("pressed");
+
     return (
-        <View>
+        <View style={styles.contener} >
             <Text> Hello volunteer,  {currentUser.email}  !!!</Text>
+
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    contener: {
+        flex: 1,
+        backgroundColor: "peachpuff",
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingTop: Platform.OS === "android" ? 25 : 0,
+    },
+    row: {
+        justifyContent: "space-around",
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+    button: {
+        display: "flex",
+        marginTop: 15,
+        height: 140,
+        width: 140,
+        backgroundColor: "red"
+    }
+
+});
 
 export default HomepageVolunteer;
