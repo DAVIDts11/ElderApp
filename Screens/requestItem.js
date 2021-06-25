15e62995055639b3d43a5c27e20c0ff05d16a138
DAVID <tsiba1@walla.co.il>
2021-06-25 19:03:09 +0300

    member home,  View

--- Screens/requestItem.js
+++ Screens/requestItem.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React ,{useState ,useEffect} from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { format } from "date-fns";
+import { useDispatch, useSelector } from "react-redux";
 
 const Request = (props) => {
+    const { currentUser } = useSelector((state) => state.user);
+
+    const [date, setDate]=useState();
+        useEffect(() => {       
+        setDate(new Date(props.req.childObj.date)); 
+        console.log("dat  == ", String(date));
+        return () => { }
+    }, [])
 
     console.log("req props === > ", props.req)
     function onprs() { }
+    
     return (
         <TouchableOpacity onPress={onprs}>
             <View style={styles.contener}>
                 <View>
                     <Text>{props.req.childObj.name}</Text>
                     <Text>{props.req.childObj.amount} </Text>
+                    <Text> {String(date)} </Text>
                 </View>
             </View>
         </TouchableOpacity>
-
     )
 }
 
